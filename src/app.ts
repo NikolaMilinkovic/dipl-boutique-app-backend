246685321731b068c71fa65a8bf65937ec370332
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2025-03-14 13:54:00 +0100

    feat: initial project setup

---
+++ src/app.ts
@@ -0,0 +1,54 @@
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+// view engine setup
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Kako bih skratio relativne putanje mogu da koristim #
+// Ovo mi omogucava postavka u package.json
+// "imports": {
+//   "#*": "./src/*"
+// },
+import indexRouter from "#routes/index.js";
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: process.env.NODE_ENV === "development" ? err : {},
+  });
+});
+
+console.log("> Application started.");
+
+export default app;
---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log('> index route called')
+  res.send(":)");
+});
+
+export default router;
